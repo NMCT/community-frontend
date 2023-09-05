096a24b1d85aa18ebe646cbf8640e8c50621100c
jarnedel <jarne.delarue@student.howest.be>
2023-09-05 15:17:53 +0200

    Migrate to unocss

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
 import { createApp } from 'vue'
 
+// Uno css framework
+import 'virtual:uno.css'
+import '@unocss/reset/normalize.css'
+
+
 import { defaultConfig, plugin } from '@formkit/vue'
 import config from '../formkit.config.ts'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import './assets/style.css'
 import App from '@/App.vue'
 
 import { useFirebase } from '@/composables/useFirebase.ts'
 import { router } from '@/bootstrap/router.ts'
-import { useFirstLogin } from '@/composables/useFirstLogin.ts'
+import { useFirstLogin } from '@/composables/useFirstLogin'
 
-const { restoreLogin, MicrosoftLoginResult } = useFirebase()
+const { restoreLogin } = useFirebase()
 const { postFirstLogin } = useFirstLogin()
 
 const create = () => {
@@ -30,7 +34,6 @@ const create = () => {
     //     theme: 'tailwindcss/genesis',
     // }))
     .use(plugin, defaultConfig(config))
-    .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
 }
 
